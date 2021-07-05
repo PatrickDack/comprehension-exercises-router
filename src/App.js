0fcd0c7a1b66b7d5134efad9a45673edceb87b95
Patrick Dack <patrickdack89@gmail.com>
2021-07-05 15:28:31 -0300

    Exercicios 1 e 2 - Encapsula a aplicaçao e cria uma rota para Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route path="/" component={Home} />
+      </BrowserRouter>
     );
   }
 }
